70964f9499bf44d814b75890375ee12029a5cbdb
Ziang Zhou <zz188@duke.edu>
2022-03-06 03:48:21 -0500

    update latest results in env

--- serverless_handler.py
+++ serverless_handler.py
@@ -6,21 +6,26 @@ def handler(data: dict, context: object) -> Dict[str, Any]:
     
     records = {}
     cpu_env = {}
-    # for the first time, cpu_env will be initialized, afterwards it can be directly retrieved and updated
-    if getattr(context, "cpu_env", None) is None:
+    # for the first time, env will be initialized, afterwards it can be directly retrieved and updated
+    if getattr(context, "env", None) is None:
         n_cpus = 0
         for i in range(100):
             if not "cpu_percent-%d"%i in data:
                 break
             n_cpus += 1
             
-    assert n_cpus > 0, "problematic data stream stored in redis. Please contract maintainer for assistance"
-    cpu_env["cpu_count"] = n_cpus
+        assert n_cpus > 0, "problematic data stream stored in redis. Please contract maintainer for assistance"
+        context.env = cpu_env
+        
+    cpu_env = context.env
+    n_cpus = cpu_env.n_cpus
+
     for i in range(n_cpus):
-        # cpu_env[i].append(data['cpu_percent-%d'%i])
         curr_data = data['cpu_percent-%d'%i]
         records['cpu_%d_1min_avg'] = np.mean(curr_data[-12:])
+        cpu_env['cpu_%d_1min_avg'] = np.mean(curr_data[-12:])
         records['cpu_%d_1h_avg'] = np.mean(curr_data[-720:])
+        cpu_env['cpu_%d_1h_avg'] = np.mean(curr_data[-720:])
     
     records['VM_used_past_5min_avg'] = np.mean(data['virtual_memory-percent'][-60:])
     
